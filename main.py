d3f96eec9e470fd0f9da34d1c8c13d99fa1e9921
Kishan Joshi <robokishan.blogspot@gmail.com>
2021-05-28 01:36:32 +0530

    particle_subscribe function added for generic events

--- main.py
+++ main.py
@@ -16,12 +16,10 @@ PARTICLE_ACCESS_TOKEN = os.getenv('PARTICLE_ACCESS_TOKEN', "test")
 PARTICLE_EVENT_NAME = "deviceLocator"
 
 def getLocation(cell_tower_info):
-    # print(f"Getting Location: {cell_tower_info}")
     x = requests.post(GOOGLE_API, params={"key":GOOGLE_API_KEY}, json = cell_tower_info)
     return x.json()
 
 def get_device(macid):
-    # print(f"Getting details {macid}")
     x = requests.get(PARTICLE_DEVICE+macid, params= { "access_token": PARTICLE_ACCESS_TOKEN })
     return x.json()
 
@@ -35,7 +33,6 @@ def convert_from_particle_format(particle_format):
     except Exception as e:
         print(format_exc())
     # print(f"Format : { 'Particle' if _is_p_format == True else 'Simple' } ")
-
     if _is_p_format == True:
         cell_towers = []
         for towers in particle_format['c']['a']:
@@ -50,37 +47,47 @@ def convert_from_particle_format(particle_format):
         data = particle_format
     return data
 
+
+def particle_subscribe(callback,event):
+    if len(PARTICLE_ACCESS_TOKEN) > 1:
+        url = f"{PARTICLE_CLOUD_EVENT}"
+        if len(event) > 1:
+            url+=f"/{event}"
+        print(f"Listeing on {url}")
+        r = requests.get(url, params= { "access_token": PARTICLE_ACCESS_TOKEN } ,stream=True)
+        for chunk in r.iter_content(chunk_size=1024):
+            _event = chunk.decode('utf8').replace("'", '"')
+            _event = _event.split("\n")
+            try:
+                callback(_event)
+            except Exception as e:
+                print(format_exc())
+    else:
+        print("Please Provide Particle access_token")
+        exit(1)
+
+def run_location(event_response):
+    if len(event_response[0].split(": ")) > 1:
+        event_name = event_response[0].split(": ")[1]
+        event_data = event_response[1].split(": ")[1]
+        event_data = json.loads(event_data)
+        cell_tower_data = json.loads(event_data['data'])
+        cell_tower_data = convert_from_particle_format(cell_tower_data)
+        location = getLocation(cell_tower_data)
+        device = get_device(event_data['coreid'])
+        print(f"Name :{device['name']} {location['location']['lat']},{location['location']['lng']} Location: {location}")
+
 if __name__ == "__main__":
+
+    # Get data from argument json
     if len(sys.argv) > 1:
         data = json.loads(sys.argv[1])
         data = convert_from_particle_format(data)
         getLocation(data)
-    else:
+    else: #listen from the particle channel
         particle_cloud = None
-        if len(PARTICLE_ACCESS_TOKEN) > 1:
-            url = f"{PARTICLE_CLOUD_EVENT}"
-            if len(PARTICLE_EVENT_NAME) > 1:
-                url = f"{PARTICLE_CLOUD_EVENT}/{PARTICLE_EVENT_NAME}"
-            print(f"Listeing on {url}")
-            r = requests.get(url, params= { "access_token": PARTICLE_ACCESS_TOKEN } ,stream=True)
-            for chunk in r.iter_content(chunk_size=1024):
-                _event = chunk.decode('utf8').replace("'", '"')
-                _event = _event.split("\n")
-                if len(_event[0].split(": ")) > 1:
-                    event_name = _event[0].split(": ")[1]
-                    event_data = _event[1].split(": ")[1]
-                    event_data = json.loads(event_data)
-                    try:
-                        cell_tower_data = json.loads(event_data['data'])
-                        cell_tower_data = convert_from_particle_format(cell_tower_data)
-                        location = getLocation(cell_tower_data)
-                        device = get_device(event_data['coreid'])
-                        print(f"Name :{device['name']} {location['location']['lat']},{location['location']['lng']} Location: {location}")
-                        # print(f"{event_name}: {(event_data)}")
-                    except Exception as e:
-                        # print(e)
-                        print(format_exc())
-                        # print(f"{event_name}: {(event_data)}")
+        particle_subscribe(run_location, PARTICLE_EVENT_NAME)
+
 
     
     
